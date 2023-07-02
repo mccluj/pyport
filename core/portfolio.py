156543083ab19dd103eb23c010ef5a0d9fa890f2
mccluj <mccluj@verizon.net>
2023-07-02 02:11:01 -0400

    rebalance looks ok

--- core/portfolio.py
+++ core/portfolio.py
@@ -6,22 +6,47 @@ class Portfolio:
     def __init__(self, cash):
         self.cash = cash
         self.holdings = pd.Series(dtype=float)
+        self.prices = pd.Series(dtype=float)
         self.trades = pd.DataFrame()
-        self.aum = cash
 
     def rebalance(self, target, context):
-        """Update holdings with new target shares
+        """Update holdings with new target shares. Update cash with with trade value, where
+        trades assumed executed at context market prices.
         :param target: pd.Series -- target shares by asset
         :param context: dict - market data, asset definitions
         :return: None
         """
         date = context['market']['date']
-        target = pd.Series(target)
-        prices = pd.Series(context['market']['spot_prices']).reindex(target.index)
-        # compute trades
-        # return trade_summary object
-        
-    def reprice(self, context):
-        """Reprice all assets, update cash with dividends, and return performance_summary object"""
-        
+        prices = context['market']['spot_prices']
+        trade_shares = target.sub(self.holdings, fill_value=0)
+        Portfolio.check_for_missing_prices(trade_shares, prices)
+        self.prices = prices.reindex(trade_shares.index)
+        self.trades = pd.DataFrame({'shares': trade_shares, 'price': self.prices})
+        self.holdings = target
+        self.cash -= self.trades.shares @ self.trades.price
 
+    def reprice(self, context):
+        """Reprice all assets, update cash with dividends.
+        :param context: dict
+        :return None
+        """
+        prices = context['market']['spot_prices']
+        Portfolio.check_for_missing_prices(self.holdings, prices)
+        self.prices = prices.reindex(self.holdings)
+        dividends = context['market']['dividends']
+        dividend_value = dividends.mul(self.holdings, fill_value=0).sum()
+        self.cash += dividend_value
+
+    def to_string(self):
+        return f'Portfolio(cash={self.cash}, aum={self.aum}):\nholdings={self.holdings}'
+
+    @staticmethod
+    def check_for_missing_prices(series, prices):
+        prices = prices.reindex(series.index)
+        missing = prices[prices.isna()].index.to_list()
+        if len(missing) > 0:
+            raise ValueError(f'Missing prices for {missing}')
+        
+    @property
+    def aum(self):
+        return self.cash + self.holdings @ self.prices
--- tests/core/test_portfolio.py
+++ tests/core/test_portfolio.py
@@ -7,16 +7,26 @@ from pyport import Portfolio, Stock
 
 class TestPortfolio(unittest.TestCase):
     def setUp(self):
+        self.cash = 10000
+        self.prices = pd.Series({'SPY': 400, 'AGG': 50})
+        self.dividends = pd.Series({'SPY': 1})
         self.context = {'assets': {'SPY': Stock('SPY'),
                                    'AGG': Stock('AGG')},
                         'market': {'date': '1/1/2023',
-                                   'spot_prices': {'SPY': 400, 'AGG': 50},
-                                   'dividends': {'SPY': 1}},
+                                   'spot_prices': self.prices,
+                                   'dividends': self.dividends},
                         }
         self.target = pd.Series({'SPY': 10, 'AGG': 50})
-        self.portfolio = Portfolio(cash=10000)
+        self.portfolio = Portfolio(cash=self.cash)
 
     def test_initial_portfolio(self):
-        assert self.portfolio.cash == 10000
+        assert self.portfolio.cash == self.cash
+        assert self.portfolio.aum == self.cash
         assert self.portfolio.holdings.empty
         assert self.portfolio.trades.empty
+
+    def test_rebalance(self):
+        self.portfolio.rebalance(self.target, self.context)
+        target_value = self.prices @ self.target
+        assert self.portfolio.aum == self.cash
+        assert self.portfolio.cash == self.cash - target_value
