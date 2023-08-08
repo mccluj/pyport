ec41a717e5bec90475ecd485c17b23d070827dbe
mccluj <mccluj@verizon.net>
2023-08-08 09:06:33 -0400

    Update rate_engine.py

--- examples/rate_engine.py
+++ examples/rate_engine.py
@@ -1,7 +1,3 @@
-import numpy as np
-from scipy.interpolate import interp1d
-import pandas as pd
-
 class RateEngine:
     """
     A class for interpolating discount rates based on date and tenor in days.
@@ -19,12 +15,19 @@ class RateEngine:
 
     def __init__(self, discount_rates):
         self.discount_rates = discount_rates
-        self.interp_func = None
-        self.update_interpolator()
+        self.interp_functions = self._create_interp_functions()
 
-    def update_interpolator(self):
-        self.interp_func = interp1d(self.discount_rates['tenor'], self.discount_rates['rate'],
-                                    kind='linear', fill_value='extrapolate')
+    def _create_interp_functions(self):
+        interp_functions = {}
+
+        def create_interp_function(group):
+            return interp1d(group['tenor'], group['rate'], kind='linear', fill_value='extrapolate')
+
+        grouped = self.discount_rates.groupby('date')
+        for date, group in grouped:
+            interp_functions[date] = create_interp_function(group)
+
+        return interp_functions
 
     def get_rate(self, date, tenor):
         """
@@ -42,24 +45,7 @@ class RateEngine:
         float
             The interpolated discount rate.
         """
-        matching_rates = self.discount_rates[self.discount_rates['date'] == date]
-        if len(matching_rates) > 0:
-            return self.interp_func(tenor)
+        if date in self.interp_functions:
+            return self.interp_functions[date](tenor)
         else:
             raise ValueError(f"No matching rate data found for date: {date}")
-
-
-# Sample DataFrame with date, tenor, and rate columns
-data = {
-    'date': ['2023-08-01', '2023-08-02', '2023-08-03'],
-    'tenor': [30, 60, 90],
-    'rate': [0.02, 0.025, 0.03]
-}
-discount_rates_df = pd.DataFrame(data)
-
-# Create an instance of RateEngine
-engine = RateEngine(discount_rates_df)
-
-# Test the interpolation
-interpolated_rate = engine.get_rate('2023-08-01', 75)
-print(f"Interpolated rate: {interpolated_rate:.5f}")
