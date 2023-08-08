5c11dc69bb8fe0f3f330f9647c064b13c8e10b7a
mccluj <mccluj@verizon.net>
2023-08-08 09:17:41 -0400

    Update rate_engine.py

--- examples/rate_engine.py
+++ examples/rate_engine.py
@@ -1,51 +1,39 @@
+import pandas as pd
+from scipy.interpolate import interp1d
+
 class RateEngine:
-    """
-    A class for interpolating discount rates based on date and tenor in days.
+    def __init__(self, dataframe):
+        # Assuming the DataFrame has columns 'date', 'days', and 'rate'
+        self.df = dataframe
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df.sort_values(by=['date', 'days'], inplace=True)
+    
+    def get_rate(self, target_date):
+        target_date = pd.to_datetime(target_date)
+        # Get the rows for the specific date
+        filtered_data = self.df[self.df['date'] == target_date]
+        
+        if filtered_data.empty:
+            return None
+        
+        interpolation_function = interp1d(filtered_data['days'], filtered_data['rate'], kind='linear', fill_value="extrapolate")
+        
+        # You can interpolate based on the days you want
+        desired_days = filtered_data['days'].iloc[0] # Modify this as needed
+        rate = interpolation_function(desired_days)
+        
+        return rate
 
-    Parameters:
-    ----------
-    discount_rates : pd.DataFrame
-        A DataFrame containing 'date', 'tenor', and 'rate' columns.
+# Sample DataFrame
+data = {'date': ['2022-01-01', '2022-01-01', '2022-06-01', '2022-06-01', '2022-12-01', '2022-12-01'],
+        'days': [0, 5, 150, 155, 365, 370],
+        'rate': [2.0, 2.1, 2.5, 2.6, 3.0, 3.1]}
 
-    Methods:
-    --------
-    get_rate(date: str, tenor: float) -> float:
-        Returns the interpolated discount rate for the given date and tenor in days.
-    """
+df = pd.DataFrame(data)
 
-    def __init__(self, discount_rates):
-        self.discount_rates = discount_rates
-        self.interp_functions = self._create_interp_functions()
+engine = RateEngine(df)
 
-    def _create_interp_functions(self):
-        interp_functions = {}
-
-        def create_interp_function(group):
-            return interp1d(group['tenor'], group['rate'], kind='linear', fill_value='extrapolate')
-
-        grouped = self.discount_rates.groupby('date')
-        for date, group in grouped:
-            interp_functions[date] = create_interp_function(group)
-
-        return interp_functions
-
-    def get_rate(self, date, tenor):
-        """
-        Get the interpolated discount rate for the given date and tenor in days.
-
-        Parameters:
-        -----------
-        date : str
-            The date for which the discount rate needs to be interpolated (YYYY-MM-DD).
-        tenor : float
-            The tenor in days for which the discount rate needs to be interpolated.
-
-        Returns:
-        --------
-        float
-            The interpolated discount rate.
-        """
-        if date in self.interp_functions:
-            return self.interp_functions[date](tenor)
-        else:
-            raise ValueError(f"No matching rate data found for date: {date}")
+# Get the rate for a given date
+date = '2022-06-01'
+rate = engine.get_rate(date)
+print(f"Interpolated rate for {date}: {rate}")
