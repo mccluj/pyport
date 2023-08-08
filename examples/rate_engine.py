744dcf9e9fd4066a500c5f9b6f9a8d3239385d62
mccluj <mccluj@verizon.net>
2023-08-08 08:50:17 -0400

    Update rate_engine.py

--- examples/rate_engine.py
+++ examples/rate_engine.py
@@ -1,82 +1,65 @@
 import numpy as np
 from scipy.interpolate import interp1d
+import pandas as pd
 
 class RateEngine:
     """
-    A class for interpolating discount rates based on tenor in days.
+    A class for interpolating discount rates based on date and tenor in days.
 
     Parameters:
     ----------
-    discount_rates : dict
-        A dictionary containing tenor in days as keys and corresponding discount rates as values.
-
-    Attributes:
-    -----------
-    tenors : array_like
-        An array containing the sorted tenor values.
-    rates : array_like
-        An array containing the corresponding sorted discount rate values.
-    interp_func : scipy.interpolate.interp1d
-        The interpolation function based on the input discount rates.
+    discount_rates : pd.DataFrame
+        A DataFrame containing 'date', 'tenor', and 'rate' columns.
 
     Methods:
     --------
-    get_rate(tenor: float) -> float:
-        Returns the interpolated discount rate for the given tenor in days.
+    get_rate(date: str, tenor: float) -> float:
+        Returns the interpolated discount rate for the given date and tenor in days.
     """
 
     def __init__(self, discount_rates):
-        self.tenors, self.rates = zip(*sorted(discount_rates.items()))
-        self.interp_func = interp1d(self.tenors, self.rates, kind='linear', fill_value='extrapolate')
+        self.discount_rates = discount_rates
+        self.interp_func = None
+        self.update_interpolator()
 
-    def get_rate(self, tenor):
+    def update_interpolator(self):
+        self.interp_func = interp1d(self.discount_rates['tenor'], self.discount_rates['rate'],
+                                    kind='linear', fill_value='extrapolate')
+
+    def get_rate(self, date, tenor):
         """
-        Get the interpolated discount rate for the given tenor in days.
+        Get the interpolated discount rate for the given date and tenor in days.
 
         Parameters:
         -----------
+        date : str
+            The date for which the discount rate needs to be interpolated (YYYY-MM-DD).
         tenor : float
-            The tenor for which the discount rate needs to be interpolated.
+            The tenor in days for which the discount rate needs to be interpolated.
 
         Returns:
         --------
         float
             The interpolated discount rate.
         """
-        return self.interp_func(tenor)
+        matching_rates = self.discount_rates[self.discount_rates['date'] == date]
+        if len(matching_rates) > 0:
+            return self.interp_func(tenor)
+        else:
+            raise ValueError(f"No matching rate data found for date: {date}")
 
 
-# Unit tests for RateEngine class
-import unittest
+# Sample DataFrame with date, tenor, and rate columns
+data = {
+    'date': ['2023-08-01', '2023-08-02', '2023-08-03'],
+    'tenor': [30, 60, 90],
+    'rate': [0.02, 0.025, 0.03]
+}
+discount_rates_df = pd.DataFrame(data)
 
-class TestRateEngine(unittest.TestCase):
-    def test_interpolation(self):
-        # Test interpolation with sample data
-        discount_rates = {30: 0.02, 60: 0.025, 180: 0.03, 365: 0.035}
-        engine = RateEngine(discount_rates)
+# Create an instance of RateEngine
+engine = RateEngine(discount_rates_df)
 
-        # Interpolate within the data range
-        self.assertAlmostEqual(engine.get_rate(90), 0.0275, places=4)
-
-        # Extrapolate below the data range (flat extrapolation)
-        self.assertAlmostEqual(engine.get_rate(15), 0.02, places=4)
-
-        # Extrapolate above the data range (flat extrapolation)
-        self.assertAlmostEqual(engine.get_rate(500), 0.035, places=4)
-
-    def test_single_rate(self):
-        # Test when there's only one rate in the data
-        discount_rates = {90: 0.03}
-        engine = RateEngine(discount_rates)
-
-        # Interpolate at the single data point (should return the same rate)
-        self.assertAlmostEqual(engine.get_rate(90), 0.03, places=4)
-
-        # Extrapolate below the data range (flat extrapolation)
-        self.assertAlmostEqual(engine.get_rate(30), 0.03, places=4)
-
-        # Extrapolate above the data range (flat extrapolation)
-        self.assertAlmostEqual(engine.get_rate(180), 0.03, places=4)
-
-if __name__ == '__main__':
-    unittest.main()
+# Test the interpolation
+interpolated_rate = engine.get_rate('2023-08-01', 75)
+print(f"Interpolated rate: {interpolated_rate:.5f}")
