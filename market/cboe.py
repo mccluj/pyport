cfd39faf479d369ace2e83e075540953ad649d34
mccluj <mccluj@verizon.net>
2023-09-16 08:45:58 -0400

    cboe.py -- add delta and data cache short_puts.py -- add delta and gearing to output simulatel_one_contract.py -- just the basics

--- market/cboe.py
+++ market/cboe.py
@@ -11,6 +11,8 @@ class CBOEMarket:
         index_columns = ['quote_date', 'underlying_symbol', 'root', 'option_type', 'expiration', 'strike']
         self._data = data
         self._indexed_data = data.set_index(index_columns).sort_index()
+        self._data_cache = {}
+        self._deltas_cache = {}
         self._quotes_cache = {}
         self._underlying_quotes_cache = {}
 
@@ -64,6 +66,18 @@ class CBOEMarket:
             self._quotes_cache[option] = quotes
         return quotes.loc[date]
 
+    def get_delta(self, date, option):
+        """
+        :param date: datetime object
+        :param option: CBOEOption
+        :return: tuple (bid_price, ask_price)
+        """
+        deltas = self._deltas_cache.get(option)
+        if deltas is None:
+            deltas = self._find_data(option).set_index('quote_date')['delta_1545']
+            self._deltas_cache[option] = deltas
+        return deltas.loc[date]
+
     def get_underlying_quote(self, date, underlying_symbol):
         """
         :param date: datetime object
@@ -80,13 +94,16 @@ class CBOEMarket:
         return quotes.loc[date]
 
     def _find_data(self, option):
-        index = pd.IndexSlice[:,  # skip quote_date
-                              option.underlying_symbol,
-                              option.root,
-                              option.option_type.value,
-                              option.expiration,
-                              option.strike]
-        data = self._indexed_data.loc[index, :].reset_index()
+        data = self._data_cache.get(option)
+        if data is None:
+            index = pd.IndexSlice[:,  # skip quote_date
+                                  option.underlying_symbol,
+                                  option.root,
+                                  option.option_type.value,
+                                  option.expiration,
+                                  option.strike]
+            data = self._indexed_data.loc[index, :].reset_index()
+            self._data_cache[option] = data
         return data
 
     @staticmethod
--- strategies/simulate_one_contract.py
+++ strategies/simulate_one_contract.py
@@ -7,26 +7,25 @@ from pyport.core.cboe_option import CBOEOption, OptionType, calculate_payoff
 from pyport.market.cboe import CBOEMarket
 
 
-# path = '~/data/cboe/UnderlyingOptionsEODCalcs.pkl'
-# market = CBOEMarket.from_pickle(path)
-path = '~/data/cboe/UnderlyingOptionsEODCalcs_2022-08.zip'
-market = CBOEMarket.from_csv(path)
+path = '~/data/cboe/UnderlyingOptionsEODCalcs.pkl'
+market = CBOEMarket.from_pickle(path)
+# path = '~/data/cboe/UnderlyingOptionsEODCalcs_2022-08.zip'
+# market = CBOEMarket.from_csv(path)
 
 config = dict(
     underlying_symbol='^SPX',
     root='SPXW',
     moneyness=1.0,
-    option_type='P',
+    option_type='C',
     tenor_days=7,
 )
-initial_aum = 10000
-participation = -0.1
 
-def simulate_strategy(market, config, initial_aum, participation):
-    aum = initial_aum
-    results = pd.DataFrame(0.0, index=market.date_range(), columns=['spot', 'holdings', 'daily_pnl', 'delta', 'gearing', 'aum'])
+def simulate_strategy(market, config):
+    results = pd.DataFrame(0.0, index=pd.Index(market.date_range(), name='Date'),
+                           columns=['spot', 'price', 'delta', 'daily_pnl'])
     daily_pnl = 0                   # only needed until the first contract is created.
     contract = None
+    n_contracts = 1
     for date in market.date_range():
         spot = market.get_underlying_quote(date, config['underlying_symbol'])['mid']
         if contract is not None:
@@ -35,8 +34,8 @@ def simulate_strategy(market, config, initial_aum, participation):
                 value = n_contracts * contract_payoff
                 contract_pnl = n_contracts * (contract_payoff - initial_price)
                 daily_pnl = n_contracts * (contract_payoff - previous_price)
-                logging.debug("%s %.2f: EXP %.2f shs @ %.2f = %.2f pnl: %.2f aum: %.2f",
-                              date.date(), spot, n_contracts, contract_payoff, value, contract_pnl, aum + daily_pnl)
+                logging.debug("%s %.2f: EXP %.2f shs @ %.2f = %.2f pnl: %.2f",
+                              date.date(), spot, n_contracts, contract_payoff, value, contract_pnl)
                 contract = None
             else:
                 current_price = market.get_quote(date, contract)['ask']
@@ -44,36 +43,21 @@ def simulate_strategy(market, config, initial_aum, participation):
         if contract is None:
             contract = market.find_option(date=date, spot=spot, **config)
             initial_price = market.get_quote(date, contract)['bid']
-            n_contracts = participation * np.abs(aum) / initial_price
             value = n_contracts * initial_price
-            logging.debug("%s %.2f: ACQ %.2f shs @ %.2f = %.2f aum: %.2f %s",
-                          date.date(), spot, n_contracts, initial_price, value, aum, contract.as_tuple())
+            logging.debug("%s %.2f: ACQ %.2f shs @ %.2f = %.2f",
+                          date.date(), spot, n_contracts, initial_price, value, contract.as_tuple())
             current_price = initial_price
-        aum += daily_pnl
         delta = market.get_delta(date, contract)
-        gearing = spot / current_price
-        results.loc[date] = (spot, n_contracts, daily_pnl, delta, gearing, aum)
+        results.loc[date] = (spot, current_price, delta, daily_pnl)
         previous_price = current_price
         print(f'{date} spot={spot:8.2f} price={current_price:8.2f} aum={aum:8.2f}', flush=True)
-    results['underlying_aum'] = 10000 * results['spot'] / results['spot'].iloc[0]
     return results
 
 
-def compare_leverage_ratios():
-    summary = {}
-    for participation in np.arange(-0.1, 0.01, 0.01):
-        key = f'{participation:.0%}'
-        print(f'leverage: {key}')
-        results = simulate_strategy(market, config, initial_aum, participation)
-        summary[key] = results.aum
-
-    pd.DataFrame(summary).to_csv('short_puts.csv')
-
-
 def main():
-    results = simulate_strategy(market=market, config=config, initial_aum=10000, participation=-0.10)
-    results.to_csv('short_10%_put.csv')
-    # print(results)
+    results = simulate_strategy(market=market, config=config, initial_aum=10000)
+    results.to_csv('simulate_one_contract.csv')
+    print(results)
 
 
 if __name__ == '__main__':
