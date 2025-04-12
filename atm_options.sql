b5c20bba9a6818642c01d5567b71b1d37268e3c7
mccluj <mccluj@verizon.net>
2025-04-12 12:31:46 -0400

    Update atm_options.sql

--- atm_options.sql
+++ atm_options.sql
@@ -1,43 +1,70 @@
 WITH ThirdFridays AS (
-    SELECT DISTINCT TradeDate
+    SELECT DISTINCT date
     FROM SecurityPrices
     WHERE 
-        DAY(TradeDate) BETWEEN 15 AND 21
-        AND DATEPART(WEEKDAY, TradeDate) = 6 -- Friday
+        DAY(date) BETWEEN 15 AND 21
+        AND DATEPART(WEEKDAY, date) = 6 -- Friday
 ),
-ThirdFridaysWithNext AS (
-    SELECT 
-        TF.TradeDate AS CurrentDate,
-        MIN(TF2.TradeDate) AS NextThirdFriday
+CurrentDates AS (
+    -- For each expected 3rd Friday, find the most recent available trading day in SecurityPrices
+    SELECT TF.date AS NominalDate,
+           SP.date AS CurrentDate
     FROM ThirdFridays TF
-    JOIN ThirdFridays TF2 ON TF2.TradeDate > TF.TradeDate
-    GROUP BY TF.TradeDate
+    OUTER APPLY (
+        SELECT TOP 1 date
+        FROM SecurityPrices
+        WHERE date <= TF.date
+        ORDER BY date DESC
+    ) SP
+),
+NextThirdFridays AS (
+    -- Pair each current date with the next 3rd Friday
+    SELECT 
+        CD.CurrentDate,
+        MIN(NTF.date) AS NextNominalExpiration
+    FROM CurrentDates CD
+    JOIN ThirdFridays NTF ON NTF.date > CD.NominalDate
+    GROUP BY CD.CurrentDate
+),
+AdjustedExpirations AS (
+    -- Find the closest available expiration for each "Next 3rd Friday"
+    SELECT 
+        NTF.CurrentDate,
+        OP.date AS CurrentOptionDate,
+        OP.expiration AS ExpirationDate
+    FROM NextThirdFridays NTF
+    OUTER APPLY (
+        SELECT TOP 1 expiration, date
+        FROM OptionPrices
+        WHERE expiration <= NTF.NextNominalExpiration
+          AND date = NTF.CurrentDate
+        ORDER BY expiration DESC
+    ) OP
 ),
 ATMOptions AS (
     SELECT 
-        tf.CurrentDate,
-        tf.NextThirdFriday,
-        sp.SecurityID,
-        sp.ClosePrice,
-        op.StrikePrice,
-        op.OptionPrice,
-        op.ExpirationDate
-    FROM ThirdFridaysWithNext tf
-    JOIN SecurityPrices sp
-        ON sp.TradeDate = tf.CurrentDate
-    JOIN OptionPrices op
-        ON op.TradeDate = tf.CurrentDate
-        AND op.ExpirationDate = tf.NextThirdFriday
-        AND op.SecurityID = sp.SecurityID
-        AND op.OptionType = 'C'
-    WHERE op.StrikePrice >= sp.ClosePrice
+        AE.CurrentDate,
+        AE.ExpirationDate,
+        SP.SecurityID,
+        SP.ClosePrice,
+        OP.strike,
+        OP.OptionPrice,
+        OP.callput
+    FROM AdjustedExpirations AE
+    JOIN SecurityPrices SP ON SP.date = AE.CurrentDate
+    JOIN OptionPrices OP
+        ON OP.date = AE.CurrentDate
+        AND OP.expiration = AE.ExpirationDate
+        AND OP.SecurityID = SP.SecurityID
+        AND OP.callput = 'C'
+    WHERE OP.strike >= SP.ClosePrice
 )
--- Final selection: for each date, pick the nearest OTM or ATM strike
+-- Final: Pick closest ATM/OTM call
 SELECT *
 FROM (
     SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY CurrentDate ORDER BY op.StrikePrice ASC) AS rn
-    FROM ATMOptions op
-) ranked
+           ROW_NUMBER() OVER (PARTITION BY CurrentDate ORDER BY strike ASC) AS rn
+    FROM ATMOptions
+) Ranked
 WHERE rn = 1
 ORDER BY CurrentDate;
